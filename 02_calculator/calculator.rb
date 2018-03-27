cc50317b529e8fe792246956665910fd7fcf1baa
Lawrence Archer <lawrence.archer@gmail.com>
2018-03-27 07:23:00 +0100

    finish calculator 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -10,3 +10,30 @@ end
 def sum(arr)
   arr.inject (0){|sum, n| sum + n}
 end
+
+def multiply(arr)
+  arr.inject(1){|mult, n| mult*n}
+end
+
+def power(num, pow)
+  i = 1
+  res = num
+  while i < pow
+    res *= num
+    i += 1
+
+  end
+  res
+end
+
+def factorial(num)
+  res = num
+  while num > 1
+    num -=1
+    res *= num
+  end
+  if res == 0
+    res = 1
+  end
+  res
+end
