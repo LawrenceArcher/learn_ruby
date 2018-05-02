23430815734e510d9cf368a4d9220ab342ff1255
Lawrence Archer <lawrence.archer@gmail.com>
2018-05-02 08:05:42 +0100

    progress on pig latin 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
 #write your code here
+def isVowel (char)
+	if (char=="a" || char=="e" || char=="i" || char=="o"|| char=="u")
+		true
+	end
+end
+
+def translate(input)
+	ay = "ay"
+	output = ""
+	arr = input.split(//)
+	arr.each {|x|
+		if !isVowel(x)
+			arr.push(arr.first)
+			arr.shift
+			next
+		end
+		if isVowel(x)
+			output = arr.join + ay
+			break
+		end
+	}
+	output
+end
