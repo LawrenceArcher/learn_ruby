a5724524510e53db79b7ec2e7787f1b2d4c6b104
Lawrence Archer <lawrence.archer@gmail.com>
2018-05-03 07:33:53 +0100

    finish pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,23 +2,38 @@
 def isVowel (char)
 	if (char=="a" || char=="e" || char=="i" || char=="o"|| char=="u")
 		true
+	else
+		false
 	end
 end
 
-def translate(input)
-	ay = "ay"
+def translateWord(input)
 	output = ""
 	arr = input.split(//)
-	arr.each {|x|
-		if !isVowel(x)
-			arr.push(arr.first)
-			arr.shift
-			next
-		end
-		if isVowel(x)
-			output = arr.join + ay
-			break
+	newArr = arr
+	y = 0
+	arr.each_with_index {|x, index|
+		if x == "q" && arr[index+1] == "u"
+			z = newArr.shift(index+2)
+			newArr.push(z)
+			y += 1
+		elsif isVowel(x) == true && y == 0
+			z = newArr.shift(index)
+			newArr.push(z)
+			y += 1
 		end
 	}
+	newArr.join + "ay"
+end
+
+def translate(input)
+	arr = input.split(' ')
+	outputArr = []
+	arr.each {|x|
+		outputArr.push(translateWord(x))
+	}
+	output = outputArr.join(" ")
 	output
 end
+
+puts translateWord("quiet")
