34564d5c8b8b1850da10187732250155663ceed4
Lawrence Archer <lawrence.archer@gmail.com>
2018-05-08 07:11:05 +0100

    complete book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title=(new_title)
+		arr = new_title.split(" ")
+		outputArr = []
+		arr.each_with_index {|x, index|
+			if index == 0
+				outputArr.push(x.capitalize)
+			elsif x == "a" || x == "the" || x == "an" || x == "and" || x == "from" || x == "for" || x == "in" || x == "of"
+				outputArr.push(x)
+			else
+				outputArr.push(x.capitalize)
+			end
+		}
+		@title = outputArr.join(" ")
+	end
 end
